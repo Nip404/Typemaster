55f2622777c094043d3dcd290ccd35406c558f7c
Nip404 <57417504+Nip404@users.noreply.github.com>
2019-11-05 22:41:52 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import pygame
 import random
 
 pygame.init()
-s = [800,200]
-font = pygame.font.SysFont("Garamond MS",s[0]//20)
-screen = pygame.display.set_mode(s,0,32)
+s = [800, 200]
+font = pygame.font.SysFont("Garamond MS", s[0]//20)
+screen = pygame.display.set_mode(s, 0, 32)
 
-with open("words.txt","r+") as f:
+with open("words.txt", "r+") as f:
     words = [i.split(" ")[0] for i in f.read().split("\n") if not i[0] == "-"]
 
 class WordQueue:
-    def __init__(self,words,amount):
+    def __init__(self, words, amount):
         self.words = words
         self.queue = [self._new() for _ in range(amount)]
 
@@ -26,7 +26,7 @@ class WordQueue:
 
 def main():
     while True:
-        queue = WordQueue(words,2)
+        queue = WordQueue(words, 2)
         user = ""
         
         while True:      
@@ -47,7 +47,7 @@ def main():
                     elif 97 <= event.key <= 122:
                         user += chr(event.key)
 
-                    print(user,queue.current)
+                    print(user, queue.current)
                     
             tmpg = ""
             for i in range(len(user)):
@@ -55,16 +55,16 @@ def main():
                     break
                 tmpg += user[i]
                 
-            green = font.render(tmpg,True,(0,255,0))
-            red = font.render("".join(" ".join(queue.queue)[i+len(tmpg)] for i in range(len(user)-len(tmpg))),True,(255,0,0))
-            black = font.render(" ".join(queue.queue)[len(user):],True,(0,0,0))
-            usertxt = font.render(user,True,(0,0,0))
+            green = font.render(tmpg, True, (0, 255, 0))
+            red = font.render("".join(" ".join(queue.queue)[i + len(tmpg)] for i in range(len(user) - len(tmpg))), True, (255, 0, 0))
+            black = font.render(" ".join(queue.queue)[len(user):], True, (0, 0, 0))
+            usertxt = font.render(user, True, (0, 0, 0))
             
-            screen.fill((255,255,255))
-            screen.blit(green,green.get_rect(center=[(s[0]/2)-((green.get_width()+red.get_width()+black.get_width())/2)+(green.get_width()/2),s[1]/3]))
-            screen.blit(red,red.get_rect(center=[(s[0]/2)-((green.get_width()+red.get_width()+black.get_width())/2)+green.get_width()+(red.get_width()/2),s[1]/3]))
-            screen.blit(black,black.get_rect(center=[(s[0]/2)+((green.get_width()+red.get_width()+black.get_width())/2)-(black.get_width()/2),s[1]/3]))
-            screen.blit(usertxt,usertxt.get_rect(center=[s[0]/2,s[1]*(2/3)]))
+            screen.fill((255, 255, 255))
+            screen.blit(green, green.get_rect(center=[(s[0]/2) - ((green.get_width() + red.get_width() + black.get_width())/2) + (green.get_width()/2), s[1]/3]))
+            screen.blit(red, red.get_rect(center=[(s[0]/2) - ((green.get_width() + red.get_width() + black.get_width())/2) + green.get_width() + (red.get_width()/2), s[1]/3]))
+            screen.blit(black, black.get_rect(center=[(s[0]/2) + ((green.get_width() + red.get_width() + black.get_width())/2) - (black.get_width()/2), s[1]/3]))
+            screen.blit(usertxt, usertxt.get_rect(center=[s[0]/2, s[1] * (2/3)]))
 
             pygame.display.flip()
 
